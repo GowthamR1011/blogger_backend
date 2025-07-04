bcc71599c95735e785dd8943b843a8b30ecc5330
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-07-04 20:35:05 +0100

    refactor dotenv configuration to use a constant for path definitions

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
+export const PATHS = [`.env.${process.env.NODE_ENV}.local`, ".env"];
 
-config({ path: [`.env.${process.env.NODE_ENV}.local`, ".env"] });
+config({ path: PATHS });
 
 export const { NODE_ENV, PORT, DATABASE_URL } = process.env;
