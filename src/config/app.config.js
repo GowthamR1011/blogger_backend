8863a5afce7ba4ad2a35ebc19a375c2648a9aa86
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-07-07 11:45:41 +0100

    Complete restart of the project.
    Backend will be done with nestjs

--- src/config/app.config.js
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv";
-export const PATHS = [`.env.${process.env.NODE_ENV}.local`, ".env"];
-
-config({ path: PATHS });
-
-export const { NODE_ENV, PORT, DATABASE_URL } = process.env;
